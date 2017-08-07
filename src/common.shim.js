dc93825e767411ffc72424b62d2dd024cae47f08
logicfeel <logicfeel@nate.com>
2017-08-07 18:42:26 +0900

    shim 추가

--- src/common.shim.js
+++ src/common.shim.js
@@ -36,3 +36,24 @@ if (!String.prototype.endsWith) {
         return lastIndex !== -1 && lastIndex === position;
     };
 }
+
+// iE8 지원   
+// Node.textContent  속성  innerHTML
+if (Object.defineProperty 
+  && Object.getOwnPropertyDescriptor 
+  && Object.getOwnPropertyDescriptor(Element.prototype, "textContent") 
+  && !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
+  (function() {
+    var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
+    Object.defineProperty(Element.prototype, "textContent",
+     {
+       get: function() {
+         return innerText.get.call(this);
+       },
+       set: function(s) {
+         return innerText.set.call(this, s);
+       }
+     }
+   );
+  })();
+}
